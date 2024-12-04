4ef005e76dca70b18ef4c5d3e077379794a63e7b
JUNHO AN <169124305+JunhoAn-Sasuke@users.noreply.github.com>
2024-12-04 23:06:13 +0900

    Move to new-project folder

--- new-project/src/App.tsx
+++ new-project/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./Components/Organisms/Layout/Layout";
+import Series from "./Components/Pages/Series/Series";
+import Movies from "./Components/Pages/Movies/Movies";
+import MyPage from "./Components/Pages/Mypage/Mypage";
+import PrivacyPolicy from "./Components/Pages/PrivacyPolicy/PrivacyPolicy";
+import TermsofUse from "./Components/Pages/TermsOfUse/TermsofUse";
+import ContactUs from "./Components/Pages/ContactUs/ContactUs";
+import "./App.module.sass";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <h1>Welcome to WillAn world</h1>
+                <p>Explore Series, Movies, and more!</p>
+              </div>
+            }
+          />
+          <Route path="/series" element={<Series />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-use" element={<TermsofUse />} />
+          <Route path="/contact-us" element={<ContactUs />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
-}
+};
 
 export default App;
