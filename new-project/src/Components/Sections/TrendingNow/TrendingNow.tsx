70a6c8e65eb0ea3e58fc74675258ae3288a77cd9
JUNHO AN <169124305+JunhoAn-Sasuke@users.noreply.github.com>
2024-12-17 00:05:06 +0900

    Create trendingData component and separate trendingData from TrendingNow component

--- new-project/src/Components/Sections/TrendingNow/TrendingNow.tsx
+++ new-project/src/Components/Sections/TrendingNow/TrendingNow.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import styles from './TrendingNow.module.sass';
-
-const trendingData = [
-  { id: 1, title: 'Mononoke', imageUrl: 'url-to-image-1' },
-  { id: 2, title: 'あなたの番です', imageUrl: 'url-to-image-2' },
-  { id: 3, title: 'Dune Part Two', imageUrl: 'url-to-image-3' },
-  { id: 4, title: 'The Hunt', imageUrl: 'url-to-image-4' },
-  { id: 5, title: 'Feeling Good', imageUrl: 'url-to-image-5' },
-  { id: 6, title: 'メタモン フォーゼ', imageUrl: 'url-to-image-6' },
-  { id: 7, title: 'Accident Man', imageUrl: 'url-to-image-7' },
-];
+import { trendingData } from '../../../data/trendingData';
 
 const TrendingNow: React.FC = () => {
   return (
---
+++ new-project/src/data/trendingData.ts
@@ -0,0 +1,16 @@
+export interface TrendingItem {
+    id: number;
+    title: string;
+    imageUrl: string;
+  }
+  
+  export const trendingData: TrendingItem[] = [
+    { id: 1, title: 'Mononoke', imageUrl: 'url-to-image-1' },
+    { id: 2, title: 'あなたの番です', imageUrl: 'url-to-image-2' },
+    { id: 3, title: 'Dune Part Two', imageUrl: 'url-to-image-3' },
+    { id: 4, title: 'The Hunt', imageUrl: 'url-to-image-4' },
+    { id: 5, title: 'Feelin Good', imageUrl: 'url-to-image-5' },
+    { id: 6, title: 'メタモン フォーゼ', imageUrl: 'url-to-image-6' },
+    { id: 7, title: 'Accident Man', imageUrl: 'url-to-image-7' },
+  ];
+  
